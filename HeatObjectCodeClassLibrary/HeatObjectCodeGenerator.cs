67431faf14106d0f1252cb74e8ac6af5f425a731
Alena Zhang <anna_blackcat_darkness@mail.ru>
2019-09-17 19:55:32 +0500

    Add task 1.1 without tests

---
+++ HeatObjectCodeClassLibrary/HeatObjectCodeGenerator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace HeatObjectCodeClassLibrary
+{
+    using HeatingProject.Objects;
+
+    public static class HeatObjectCodeGenerator
+    {
+        private const short maxCountOfDigits = 18;
+
+        private static void checkStr(string str, string paramName, string message)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                throw new ArgumentNullException(paramName, message);
+            }
+        }
+
+        ///<summary>
+        /// Реализация первого алгоритма:
+        /// 1 символ - код данного алгоритма (S)
+        /// Далее дата в формате ddmmyy без разделителей и только с 2 последними цифрами года
+        /// Далее наименование объекта целиком, если влезает в 18 символов
+        /// Далее разделитель "-"
+        /// Далее часть или полностью номер лицевого счета
+        /// </summary>
+        /// <remarks>
+        /// Предполагается, что наименование объекта:
+        /// - не слишком длинное
+        /// - это строка
+        /// И в целом по дате, имени и части счета можно будет узнать объект
+        /// Также предполагается, что номер счета влезает в long и не является отрицательным
+        /// </remarks>
+        /// <param name="heatObject">Информация об объекте теплопотребления. См. больше в проекте HeatingProject.Objects</param>
+        /// <returns>Строка цифро-буквенного кода</returns>
+
+        public static string GetSimpleCode(HeatObject heatObject)
+        {
+            checkStr(heatObject.objName, "Наименование объекта", "Наименование объекта не может быть пусто");
+
+            var dateStr = $"{heatObject.registryDate:dd}{heatObject.registryDate:MM}{heatObject.registryDate:yy}";
+            var resultCode = $"S{dateStr}{heatObject.objName}-{heatObject.personalAccount}";
+            resultCode = resultCode.Length > maxCountOfDigits ? resultCode.Remove(maxCountOfDigits) : resultCode;
+
+            return resultCode;
+        }
+
+        /// <summary>
+        /// Реализация второго алгоритма:
+        /// 1 символ - код данного алгоритма (H)
+        /// Далее наименование объекта (обрезано до 6 символов, если больше)
+        /// Далее разделитель "-"
+        /// Далее часть или полностью номер лицевого счета (обрезано до 6 символов)
+        /// Далее разделитель "-"
+        /// Далее дата в формате mmyy, переведенная в 16ричную систему счисления
+        /// </summary>
+        /// <remarks>
+        /// При обратном переводе даты, если получается строка из 3 цифр - нужно добавить слева ноль
+        /// </remarks>
+        /// <param name="heatObject">Информация об объекте теплопотребления. См. больше в проекте HeatingProject.Objects</param>
+        /// <returns>Строка цифро-буквенного кода</returns>
+
+        public static string GetHexCode(HeatObject heatObject)
+        {
+            const short maxNameLength = 6;
+
+            checkStr(heatObject.objName, "Наименование объекта", "Наименование объекта не может быть пусто");
+
+            var dateNum = heatObject.registryDate.Month * 100 + short.Parse(heatObject.registryDate.ToString("yy"));
+            var dateStr = dateNum.ToString("X");
+
+            var nameStr = heatObject.objName.Length > maxNameLength
+                ? heatObject.objName.Remove(maxCountOfDigits)
+                : heatObject.objName;
+
+            var personalAccStr = heatObject.personalAccount.ToString();
+            personalAccStr = personalAccStr.Length > maxNameLength
+                ? personalAccStr.Remove(maxNameLength)
+                : personalAccStr;
+
+            return $"H{nameStr}-{personalAccStr}-{dateStr}";
+        }
+    }
+}
---
+++ HeatingProject.Objects/HeatObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HeatingProject.Objects
+{
+    /// <summary>
+    /// Класс объекта для генерации кода
+    /// </summary>
+    public class HeatObject
+    {
+        /// <summary>
+        /// Наименование объекта теплопотребления
+        /// </summary>
+        public string objName { get; set; }
+        /// <summary>
+        /// Дата регистрации
+        /// </summary>
+        public DateTime registryDate { get; set; }
+        /// <summary>
+        /// Номер лицевого счета потребителя
+        /// </summary>
+        public ulong personalAccount { get; set; }
+    }
+}
