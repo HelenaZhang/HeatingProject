c29a0a807f0c687e07297e80e34a2a58e8d3adfa
Alena Zhang <anna_blackcat_darkness@mail.ru>
2019-09-18 23:41:00 +0500

    Add task 1.2

---
+++ HeatObjectCodeClassLibrary/HeatObjectCodeParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using HeatingProject.Objects;
+
+namespace HeatObjectCodeClassLibrary
+{
+    public static class HeatObjectCodeParser
+    {
+        public static HeatObject ParseCode(string code)
+        {
+            if (String.IsNullOrEmpty(code))
+            {
+                throw new ArgumentNullException("Код", "Код не может быть пустым");
+            }
+            else
+            {
+                switch (code[0])
+                {
+                    case 'S':
+                        return ParseSimple(code);
+                        break;
+                    case 'H':
+                        return ParseHex(code);
+                        break;
+                    default:
+                        throw new Exception("Код неверен");
+                        break;
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Заглушки для метода раскодирования различных алгоритмов получения кода и поиска объекта по базе
+        /// </summary>
+        /// <param name="code">Цифро-буквенный код</param>
+        /// <returns>Найденный объект</returns>
+       
+        private static HeatObject ParseSimple(string code)
+        {
+            return new HeatObject()
+            {
+                objName = "SimpleObj",
+                personalAccount = 468,
+                registryDate = new DateTime(2018, 12, 1)
+            };
+        }
+
+        private static HeatObject ParseHex(string code)
+        {
+            return new HeatObject()
+            {
+                objName = "HexObj",
+                personalAccount = 5678,
+                registryDate = new DateTime(2030, 6, 4)
+            };
+        }
+    }
+
+    
+}
---
+++ HeatingProject/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Web.Mvc;
+using HeatingProject.Objects;
+using HeatObjectCodeClassLibrary;
+
+namespace HeatingProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetCode()
+        {
+            ViewBag.Message = "Получение кода";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GetCode(FormCollection codeCollection)
+        {
+            DateTime date;
+            ulong account;
+            if (!DateTime.TryParse(ViewBag.RegDate = codeCollection["regDate"], out date))
+            {
+                ViewBag.Answer = "Неправильная дата";
+                return View();
+            }
+
+            if (!ulong.TryParse(ViewBag.PersAcc = codeCollection["persAcc"], out account))
+            {
+                ViewBag.Answer = "Счет должен быть целым числом!";
+                return View();
+            }
+
+            var heatObj = new HeatObject()
+            {
+                objName = ViewBag.ObjName = codeCollection["objName"],
+                personalAccount = account,
+                registryDate = date
+            };
+
+            try
+            {
+                switch (codeCollection["algoType"])
+                {
+                    case "Simple":
+                        ViewBag.Answer = HeatObjectCodeGenerator.GetSimpleCode(heatObj);
+                        break;
+                    case "Hex":
+                        ViewBag.Answer = HeatObjectCodeGenerator.GetHexCode(heatObj);
+                        break;
+                    default:
+                        ViewBag.Answer = "Ошибка!";
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Answer = ex.Message;
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ParseCode()
+        {
+            ViewBag.Message = "Поиск по коду";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ParseCode(FormCollection codeCollection)
+        {
+            HeatObject heatObj = null;
+            try
+            {
+                heatObj = HeatObjectCodeParser.ParseCode(ViewBag.Code = codeCollection["code"]);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.AnswerText = ex.Message;
+            }
+
+            if (heatObj != null) ViewBag.AnswerText = $"Наименование объекта: {heatObj.objName}\nДата регистрации: {heatObj.registryDate:dd.MM.yyyy}\nЛицевой счет: {heatObj.personalAccount}";
+            else ViewBag.AnswerText = "Ошибка!";
+
+            return View();
+        }
+    }
+}
